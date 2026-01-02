f6bf2c3a513d28a523d0c98b41d2e9904730b910
Noel Felix <nn.felix.25@gmail.com>
2026-01-02 13:01:04 -0800

    Add initial project files with Express dependency and a simple counter app
    - Created package.json with Express as a dependency
    - Added index.html with a lime green themed counter interface

---
+++ docker/aws-docker-dub-simple/app.js
@@ -0,0 +1,19 @@
+// Simple express server that serves the index.html file
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route to serve index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
