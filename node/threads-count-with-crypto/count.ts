384aef5ba69c4227b0fa2211b7c8bf54eebd2f77
Noel Felix <nn.felix.25@gmail.com>
2026-01-02 23:14:42 -0800

    fix: improve thread pool handling and update example usage in count.ts

--- node/threads-count-with-crypto/count.ts
+++ node/threads-count-with-crypto/count.ts
@@ -3,28 +3,36 @@ import { argv } from 'node:process';
 
 const start = Date.now();
 
-// Get the count arg
+const _poolSize = parseInt(process.env.UV_THREADPOOL_SIZE || '4');
+const poolSize = isNaN(_poolSize) ? 4 : _poolSize;
 
 const _count = parseInt(argv[2]);
 const count = isNaN(_count) ? 5 : _count;
-let i = 0;
+let i = 0,
+  pool = 0;
 
 while (i < count) {
-  i++;
+  if (i % poolSize === 0) {
+    pool++;
+  }
+
   pbkdf2(
     'a',
     'b',
     100000,
     512,
     'sha512',
-    function (i: number) {
-      console.log(i, Date.now() - start);
-    }.bind(this, i)
+    function (args: [number, number]) {
+      const [i, pool] = args;
+      console.log(i + 1, pool, Date.now() - start);
+    }.bind(this, [i, pool])
   );
+
+  i++;
 }
 
 // To run this example, use the following command:
-// node --threads-count=4 count.ts 10
+//  UV_THREADPOOL_SIZE=N node count.ts 10
 // The number at the end (10) is optional and indicates how many pbkdf2 operations to run.
 // If not provided, it defaults to 5.
-// You can adjust the --threads-count flag to see how it affects performance.
+// You can adjust the UV_THREADPOOL_SIZE value to see how it affects performance (libuv thread pool size).
